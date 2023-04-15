27211d5a2bd53fcf2089b2553affbf7a3ea57921
YareCanul <a.yare.canul@utponiente.edu.mx>
2023-04-14 23:42:59 -0600

    paginacion

--- src/app/Models/user-model.spec.ts
+++ src/app/Models/user-model.spec.ts
@@ -1,7 +1,20 @@
 import { UserModel } from './user-model';
+import { NgxPaginationModule } from 'ngx-pagination'; // At the top of your module
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+
 
 describe('UserModel', () => {
   it('should create an instance', () => {
     expect(new UserModel()).toBeTruthy();
   });
+
 });
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NgxPaginationModule // Add this line of code here
+  ] })
--- src/app/Models/user-model.ts
+++ src/app/Models/user-model.ts
@@ -1,3 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination'; //Imports NgxPaginationModule 
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, NgxPaginationModule ],
+  exports: [CommonModule, NgxPaginationModule], // Exports NgxPaginationModule 
+})
 export class UserModel {
 
     public prducto!: string; 
